d05fb04312a0a57debe9d5ea0e627faf0675dddb
Eduardo Huerta <xsplited002@gmail.com>
2023-03-08 19:43:11 -0600

    .

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React, {useEffect, useState} from 'react'
+import Navbar from './components/Navbar'
+import Characters from "./components/Characters"
+import Pagination from "./components/Pagination"
+
+function App() {
+
+  const initialUrl = "https://rickandmortyapi.com/api/character";
+  const [characters, setCharacters] = useState([]);
+  const [info, setInfo] = useState({});
+
+  const fetchCharacters = (url) => {
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+          setCharacters(data.results)
+          setInfo(data.info)
+      })
+      .catch(err => console.error(err))
+  }
+
+  const onPrevious = () => {
+      fetchCharacters(info.prev)
+  }
+
+  const onNext = () => {
+      fetchCharacters(info.next)
+  }
+
+  useEffect(() => {
+    fetchCharacters(initialUrl)
+  }, [])
+
+
+  return (
+      <>
+        <Navbar brand="Rick and Morty App"/>
+
+        <div className="container mt-5">
+            <Pagination
+                prev={info.prev}
+                next={info.next}
+                onPrevious={onPrevious}
+                onNext={onNext}
+            />
+            <Characters characters={characters} />
+            <Pagination
+                prev={info.prev}
+                next={info.next}
+                onPrevious={onPrevious}
+                onNext={onNext}
+            />
+        </div>
+      </>
+  );
+}
+
+export default App
